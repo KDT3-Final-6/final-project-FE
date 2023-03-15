d23491ec1a65017e70cee6e4e466cb0744e35541
team7 <hnhyerim@gmail.com>
2023-03-15 19:23:45 +0900

    Feature: 경로 상수 설정 및 적용

---
+++ src/constants/pathConst.ts
@@ -0,0 +1,14 @@
+const PATH = {
+  HOME: '/',
+  CART: '/cart',
+  CONTACT: '/contact',
+  PRODUCT: '/product',
+  REVIEW: '/review',
+  SEARCH: '/search',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  SURVEY: '/survey',
+  USER: '/user',
+}
+
+export default PATH
