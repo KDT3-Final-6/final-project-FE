3fc41fef7f28acb1292f7925942e907fc9cf8976
team7 <hnhyerim@gmail.com>
2023-03-15 19:13:36 +0900

    Feature: 푸터 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 interface Props {}
@@ -9,6 +10,7 @@ const App = (props: Props) => {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
