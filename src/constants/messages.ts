ac2feca1cd782c0f02758639267e024148345d44
team7 <hnhyerim@gmail.com>
2023-03-15 19:24:09 +0900

    Feature: 에러메세지 상수 설정

---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+const MESSAGES = {
+  LOGIN: {
+    checkEmail: '이메일을 확인해주세요.',
+    checkPW: '비밀번호를 확인해주세요.',
+  },
+  LOGOUT: {
+    error: '비정상적인 로그아웃입니다.',
+    complete: '로그아웃',
+  },
+}
+
+export default MESSAGES
