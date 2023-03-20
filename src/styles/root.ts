85c9fb144efc437915a147de6c27df277a40e3e4
team7 <hnhyerim@gmail.com>
2023-03-20 21:13:04 +0900

    Env: colors 상수 설정

---
+++ src/styles/root.ts
@@ -0,0 +1,11 @@
+const COLORS = {
+  primary: '#0088d2',
+  secondaryYellow: '#e5b741',
+  secondaryRed: '#d6512e',
+  lightGrey: '#ededed',
+  mediumGrey:'#d9d8d8',
+  black: '#090909',
+  white:'#fff'
+}
+
+export default COLORS
