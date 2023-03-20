e77e9136d84e49a0942b248310ca7609c6b2a297
team7 <hnhyerim@gmail.com>
2023-03-21 00:45:18 +0900

    Feature: Desktop 컨텐츠 너비값 생성

--- src/layout/Inner.tsx
+++ src/layout/Inner.tsx
@@ -2,15 +2,43 @@ import React from 'react'
 import styled from 'styled-components'
 
 interface Prop {
-  children: any
+  height?: string
+  display?: string
+  justifyContent?: string
+  alignItems?: string
+  children: React.ReactNode
 }
-const Inner = ({ children }: Prop) => {
-  return <InnerStyle>{children}</InnerStyle>
+const Inner = ({
+  height = '',
+  display = '',
+  justifyContent = '',
+  alignItems = 'center',
+  children,
+}: Prop) => {
+  return (
+    <InnerStyle
+      height={height}
+      display={display}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+    >
+      {children}
+    </InnerStyle>
+  )
 }
 
 export default Inner
 
-const InnerStyle = styled.div`
+const InnerStyle = styled.div<{
+  height: string
+  display: string
+  justifyContent: string
+  alignItems: string
+}>`
   width: 1180px;
+  height: ${({ height }) => height};
   margin: 0 auto;
+  display: ${({ display }) => display};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
 `
