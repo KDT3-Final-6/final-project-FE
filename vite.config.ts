c3507b4f61e701c5540b7b0c8659dbaf228badcf
Wonny-ing <h.wonny11@gmail.com>
2023-03-20 23:46:16 +0900

    Env: 절대경로 추가 및 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-});
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: '/src',
+      },
+      {
+        find: '@components',
+        replacement: '/src/components',
+      },
+      {
+        find: '@pages',
+        replacement: '/src/pages',
+      },
+      {
+        find: '@layout',
+        replacement: '/src/libs',
+      },
+      {
+        find: '@constants',
+        replacement: '/src/constants',
+      },
+      {
+        find: '@interfaces',
+        replacement: '/src/interfaces',
+      },
+    ],
+  },
+})
