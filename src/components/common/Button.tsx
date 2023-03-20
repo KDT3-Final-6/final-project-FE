13f9d731fc63a5c670606b23017ec51be0f42b6b
team7 <hnhyerim@gmail.com>
2023-03-20 21:13:44 +0900

    Feature: Button 컴포넌트 생성

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import COLORS from '@src/styles/root'
+import React, { Children } from 'react'
+import styled from 'styled-components'
 
-interface Props {}
+interface Props {
+  buttonType: string
+  width?: string
+  height?: string
+  children?: React.ReactNode
+}
 
-const Button = (props: Props) => {
-  return <div>Button</div>
+const Button = ({ buttonType = '', width = '', height = '', children }: Props) => {
+  return (
+    <ButtonStyle buttonType={buttonType} width={width} height={height}>
+      {children}
+    </ButtonStyle>
+  )
 }
 
 export default Button
+
+export const ButtonStyle = styled.button<{
+  buttonType: string
+  width: string
+  height: string
+}>`
+  ${({ buttonType }) => handleButtonType(buttonType)}
+  width:${({ width }) => width};
+  height: ${({ height }) => height};
+`
+
+const handleButtonType = (buttonType: string) => {
+  switch (buttonType) {
+    case 'skyBlue':
+      return `
+        width: 180px;
+        height: 50px;
+        border: 1px solid ${COLORS.primary};
+        color:${COLORS.primary};
+        background-color: transparent;
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+          background-color: ${COLORS.primary};
+          color: ${COLORS.white};
+        }
+      `
+    case 'transparent':
+      return `
+        width:auto;
+        height:auto;
+        background:transparent;
+      `
+    default:
+      return `
+        width: 180px;
+        height: 50px;
+        border: 1px solid ${COLORS.primary};
+        color:${COLORS.primary};
+        background-color: transparent;
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+          background-color: ${COLORS.primary};
+          color: ${COLORS.white};
+        }
+      `
+  }
+}
