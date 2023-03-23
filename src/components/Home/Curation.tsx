f49484bae1fa6add5677c0f1a68df7a749d4d096
team7 <hnhyerim@gmail.com>
2023-03-23 23:02:15 +0900

    Feature: 큐레이션 영역 상품 카드 생성

--- src/components/Home/Curation.tsx
+++ src/components/Home/Curation.tsx
@@ -2,16 +2,27 @@ import Inner from '@src/layout/Inner'
 import Section from '@src/layout/Section'
 import React from 'react'
 import styled from 'styled-components'
+import ProductCard from '../common/ProductCard'
 import Title from '../common/Title'
 
 const Curation = () => {
   return (
     <Section>
       <Inner>
-        <Title titleType="h2" fontSize="32px" title="어디로 여행을 떠날까요?" />
+        <Title titleType="h2" fontSize="32px" title="어디로 여행을 떠날까요?" marginBotton="66px" />
+        <ProductListStyle>
+          <ProductCard cardType="productCard" width="33.3333%" minHeight="500px" />
+          <ProductCard cardType="productCard" width="33.3333%" minHeight="500px" />
+          <ProductCard cardType="productCard" width="33.3333%" minHeight="500px" />
+        </ProductListStyle>
       </Inner>
     </Section>
   )
 }
 
 export default Curation
+
+const ProductListStyle = styled.ul`
+  display: flex;
+  gap: 29px;
+`
