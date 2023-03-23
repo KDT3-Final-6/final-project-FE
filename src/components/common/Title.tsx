8611cabab07110350364ec0b5108305d143defe4
team7 <hnhyerim@gmail.com>
2023-03-23 22:59:39 +0900

    Feature: title props 추가

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -5,11 +5,24 @@ interface Prop {
   titleType: string
   title: string
   fontSize?: string
+  fontWeight?: string
+  marginBotton?: string
 }
 
-const Title = ({ titleType, title, fontSize = '25px' }: Prop) => {
+const Title = ({
+  titleType,
+  title,
+  fontSize = '25px',
+  fontWeight = '700',
+  marginBotton = '',
+}: Prop) => {
   return (
-    <TitleStyle titleType={titleType} fontSize={fontSize}>
+    <TitleStyle
+      titleType={titleType}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      marginBotton={marginBotton}
+    >
       {titleType === 'h1' && <h1>{title}</h1>}
       {titleType === 'h2' && <h2>{title}</h2>}
       {titleType === 'h3' && <h3>{title}</h3>}
@@ -23,7 +36,10 @@ export default Title
 const TitleStyle = styled.div<{
   titleType: string
   fontSize: string
+  fontWeight: string
+  marginBotton: string
 }>`
   font-size: ${({ fontSize }) => fontSize};
-  font-weight: 700;
+  font-weight: ${({ fontWeight }) => fontWeight};
+  margin-bottom: ${({ marginBotton }) => marginBotton};
 `
