ef0507c81af7ae1676c1d4ffba5200463ccc8fc3
team7 <hnhyerim@gmail.com>
2023-03-27 01:25:05 +0900

    Feature: 체크박스&라디오 컴포넌트 생성

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -1,66 +1,116 @@
-import { COLORS, FONTSIZE } from '@src/styles/root'
+import { COLORS, FONTSIZE, FONTWEGHT } from '@src/styles/root'
+import { color } from 'framer-motion'
 import React from 'react'
 import styled from 'styled-components'
 
 interface ICheckBox {
+  checkboxType: string
   id?: string
+  title?: string
+  name?: string
+  labelName?: string
   children?: React.ReactNode
 }
 
-const CheckBox = ({ id, children }: ICheckBox) => {
+const CheckBox = ({ checkboxType, title, name, labelName, children }: ICheckBox) => {
   return (
-    <CheckBoxStyle>
-      <input type="checkbox" id={id} />
-      <label htmlFor={id}>{children}</label>
+    <CheckBoxStyle checkboxType={checkboxType}>
+      <p>{title}</p>
+      {children}
     </CheckBoxStyle>
   )
 }
 
 export default CheckBox
 
-const CheckBoxStyle = styled.div`
+const CheckBoxStyle = styled.div<{
+  checkboxType: string
+}>`
   display: flex;
   align-items: center;
-
-  input[type='checkbox'] {
-    width: 20px;
-    height: 20px;
-    cursor: pointer;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    -o-appearance: none;
-    outline: none;
-    content: none;
-    position: relative;
-
-    &::before {
-      content: '✔';
-      width: 100%;
-      height: 100%;
-      color: transparent;
-      display: block;
-      font-size: ${FONTSIZE.fz16};
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: end;
-      border: 1px solid ${COLORS.cddd};
-      position: absolute;
-      top: 0;
-      left: 0;
-    }
-
-    &:checked::before {
-      background-color: ${COLORS.primary};
-      color: ${COLORS.white};
-      border: none;
-    }
-  }
+  position: relative;
 
   label {
     cursor: pointer;
     padding-left: 6px;
-    font-size: ${FONTSIZE.fz12};
     color: ${COLORS.c1b1b1b};
   }
+  ${({ checkboxType }) => handleCheckbox(checkboxType)}
 `
+
+const handleCheckbox = (checkboxType: string) => {
+  switch (checkboxType) {
+    case 'checkbox':
+    case 'radio':
+      return `
+        p {
+          color:${COLORS.c1b1b1b};
+        }
+
+        input {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        -o-appearance: none;
+        outline: none;
+        content: none;
+        position: relative;
+
+        &::before {
+          content: '✔';
+          width: 100%;
+          height: 100%;
+          color: transparent;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          border: 1px solid ${COLORS.cddd};
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+
+        &[type="checkbox"]::before {
+          border-radius:4px;
+        }
+
+        &[type="radio"]::before {
+          border-radius:50px;
+        }
+
+        &:checked::before {
+          background-color: ${COLORS.primary};
+          color: ${COLORS.white};
+          border: none;
+        }
+      }
+
+      p {
+        position:absolute;
+        top:-20px;
+        left:0;
+      }
+
+      label {
+        display:flex;
+        align-items:center;
+      }
+    `
+    case 'radio':
+      return `
+        p {
+          position:absolute;
+          top:-20px;
+          left:0;
+          padding-left:0;
+        }
+
+        input {
+          width:auto;
+          height:auto;
+        }
+      `
+  }
+}
