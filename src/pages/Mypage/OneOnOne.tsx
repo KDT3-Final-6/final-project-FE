1fca8efe5b108b1abe89d06ac4e48ecb346b2de8
Wonny-ing <h.wonny11@gmail.com>
2023-03-28 16:07:19 +0900

    Feat: 마이페이지 1:1문의내용 UIUX 마크업

--- src/pages/Mypage/OneOnOne.tsx
+++ src/pages/Mypage/OneOnOne.tsx
@@ -1,9 +1,38 @@
+import Button from '@src/components/common/Button'
+import OneOnOneCard from '@src/components/MyPage/OneOnOneCard'
+import { COLORS, FONTSIZE, FONTWEGHT } from '@src/styles/root'
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {}
 
 const OneOnOne = (props: Props) => {
-  return <div>OneOnOne</div>
+  return (
+    <ContainerStyle>
+      <BtnBoxStyle>
+        <Button width="140px" buttonType="cartSkyBlue">
+          문의 작성
+        </Button>
+      </BtnBoxStyle>
+      <OneOnOneCard />
+    </ContainerStyle>
+  )
 }
 
 export default OneOnOne
+
+const ContainerStyle = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid ${COLORS.cE0E0E0};
+  padding-bottom: 30px;
+`
+
+const BtnBoxStyle = styled.section`
+  text-align: right;
+  padding: 30px 0;
+  button {
+    font-size: ${FONTSIZE.fz16};
+    font-weight: ${FONTWEGHT.fw600};
+  }
+`
