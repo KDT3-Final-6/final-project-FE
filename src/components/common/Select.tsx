e12e12c78d8712b77072a5ba9c4c6e722311b766
team7 <hnhyerim@gmail.com>
2023-03-29 02:55:18 +0900

    Feature: select 공통 컴포넌트화 수정 및 적용

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -3,8 +3,34 @@ import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 import useOnClickOutside from '../hooks/useOnClickOutside'
 
-const Select = () => {
-  const [currentValue, setCurrentValue] = useState('인기순')
+interface ISelect {
+  options: string[] | number[]
+  initial: string | number
+  value?: string | number
+  unit?: string
+  width?: string
+  height?: string
+  borderRadius?: string
+  borderColor?: string
+  isClickDefault?: boolean
+  onChange?: React.ChangeEventHandler<HTMLLIElement>
+  onClick?: React.MouseEventHandler<HTMLLIElement>
+}
+
+const Select = ({
+  options,
+  initial,
+  value,
+  unit,
+  width = '128px',
+  height = '43px',
+  borderRadius = '8px',
+  borderColor = COLORS.cb6b6b6,
+  isClickDefault = true,
+  onChange,
+  onClick,
+}: ISelect) => {
+  const [currentValue, setCurrentValue] = useState(initial)
   const [showOptions, setShowOptions] = useState(false)
   const ref = useRef(null)
   useOnClickOutside(ref, () => setShowOptions(false))
@@ -13,13 +39,35 @@ const Select = () => {
     const { innerText } = e.target as HTMLLIElement
     setCurrentValue(innerText)
   }
+
   return (
-    <SelectBoxStyle onClick={() => setShowOptions((prev) => !prev)}>
-      <LabelStyle>{currentValue}</LabelStyle>
-      <SelectOptionsStyle show={showOptions} ref={ref}>
-        <OptionStyle onClick={(e) => handleOnChangeSelectValue(e)}>인기순</OptionStyle>
+    <SelectBoxStyle
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      borderColor={borderColor}
+      isClickDefault={isClickDefault}
+      onClick={() => setShowOptions((prev) => !prev)}
+    >
+      <LabelStyle>
+        {currentValue} {unit}
+      </LabelStyle>
+      <SelectOptionsStyle show={showOptions} ref={ref} borderRadius={borderRadius}>
+        {/* <OptionStyle onClick={(e) => handleOnChangeSelectValue(e)}>인기순</OptionStyle>
         <OptionStyle onClick={(e) => handleOnChangeSelectValue(e)}>가격높은순</OptionStyle>
-        <OptionStyle onClick={(e) => handleOnChangeSelectValue(e)}>가격낮은순</OptionStyle>
+        <OptionStyle onClick={(e) => handleOnChangeSelectValue(e)}>가격낮은순</OptionStyle> */}
+        {options.map((option) => (
+          <OptionStyle
+            width={width}
+            height={height}
+            key={option}
+            value={value}
+            onClick={isClickDefault ? (e) => handleOnChangeSelectValue(e) : onClick}
+            onChange={onChange}
+          >
+            {option}
+          </OptionStyle>
+        ))}
       </SelectOptionsStyle>
     </SelectBoxStyle>
   )
@@ -27,22 +75,29 @@ const Select = () => {
 
 export default Select
 
-const SelectBoxStyle = styled.div`
+const SelectBoxStyle = styled.div<{
+  isClickDefault: boolean
+  borderColor: string
+  borderRadius: string
+  width: string
+  height: string
+}>`
   display: flex;
   align-items: center;
   position: relative;
-  width: 108px;
-  height: 43px;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
   padding: 12px 16px;
-  border-radius: 8px;
+  border-radius: ${({ borderRadius }) => borderRadius};
   background-color: ${COLORS.white};
   cursor: pointer;
-  border: 1px solid ${COLORS.cb6b6b6};
+  border: 1px solid ${({ borderColor }) => borderColor};
   &::before {
     content: '⌵';
     position: absolute;
-    top: 1px;
+    top: 42%;
     right: 16px;
+    transform: translateY(-50%);
   }
   &:hover {
     background-color: ${COLORS.cf6f6f6};
@@ -51,28 +106,36 @@ const SelectBoxStyle = styled.div`
 
 const LabelStyle = styled.label`
   font-size: ${FONTSIZE.fz14};
+  cursor: pointer;
 `
 
-const SelectOptionsStyle = styled.ul`
+const SelectOptionsStyle = styled.ul<{
+  show: boolean
+  borderRadius: string
+}>`
   position: absolute;
   list-style: none;
   top: -1px;
   left: -1px;
   width: 100%;
-  overflow: hidden;
-  height: 129px;
-  max-height: ${({ show }: { show: boolean }) => (show ? 'none' : '0')};
-  border: ${({ show }: { show: boolean }) => (show ? `1px solid ${COLORS.cb6b6b6}` : 'none')};
-  border-radius: 8px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  /* height: 129px; */
+  max-height: ${({ show }) => (show ? '300px' : '0')};
+  border: ${({ show }) => (show ? `1px solid ${COLORS.cb6b6b6}` : 'none')};
+  border-radius: ${({ borderRadius }) => borderRadius};
   background-color: ${COLORS.white};
   z-index: 10;
   box-sizing: content-box;
 `
 
-const OptionStyle = styled.li`
+const OptionStyle = styled.li<{
+  width: string
+  height: string
+}>`
   font-size: ${FONTSIZE.fz14};
-  width: 108px;
-  height: 43px;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
   padding: 12px 16px;
   display: flex;
   align-items: center;
