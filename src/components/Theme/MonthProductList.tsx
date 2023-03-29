372a2645932f23c1bae1c56708ba25c9d083b6ef
quokka-eating-carrots <ovzip7@gmail.com>
2023-03-29 15:14:58 +0900

    Feat: 카테고리 테마별 상품 페이지 마크업

---
+++ src/components/Theme/MonthProductList.tsx
@@ -0,0 +1,101 @@
+import { IProduct } from '@src/env'
+import React from 'react'
+import Title from '../common/Title'
+import styled from 'styled-components'
+import { COLORS } from '@src/styles/root'
+import CardTypeItem from '../common/CardTypeItem'
+
+type Props = {}
+
+const MonthProductList = (props: Props) => {
+  const products: IProduct[] = [
+    {
+      id: 1,
+      title: '괌 4박 5일',
+      image: 'https://cdn.imweb.me/thumbnail/20220419/31ca7e26eb12a.png',
+      hashs: ['트롤링낚시', '롯데', '패밀리스윗'],
+      price: 2135000,
+      heart: true,
+    },
+    {
+      id: 2,
+      title: '괌 4박 5일',
+      image: 'https://cdn.imweb.me/thumbnail/20220419/31ca7e26eb12a.png',
+      hashs: ['트롤링낚시', '롯데', '패밀리스윗'],
+      price: 2135000,
+      heart: true,
+    },
+    {
+      id: 3,
+      title: '괌 4박 5일',
+      image: 'https://cdn.imweb.me/thumbnail/20220419/31ca7e26eb12a.png',
+      hashs: ['트롤링낚시', '롯데', '패밀리스윗'],
+      price: 2135000,
+      heart: false,
+    },
+    {
+      id: 4,
+      title: '괌 4박 5일',
+      image: 'https://cdn.imweb.me/thumbnail/20220419/31ca7e26eb12a.png',
+      hashs: ['트롤링낚시', '롯데', '패밀리스윗'],
+      price: 2135000,
+      heart: false,
+    },
+  ]
+  return (
+    <div>
+      <Title margin="80px 0 50px 0">
+        <h2>3월 인기 테마여행 패키지</h2>
+      </Title>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '22px' }}>
+        <ProductListStyle>
+          {products.map((product) => (
+            <CardTypeItem
+              key={product.id}
+              item={product}
+              cardType="cardType"
+              imgHeight="100%"
+              height="460px"
+              priceBottom="30px"
+              priceColor={COLORS.c1b1b1b}
+            />
+          ))}
+        </ProductListStyle>
+        <ProductListStyle>
+          {products.map((product) => (
+            <CardTypeItem
+              key={product.id}
+              item={product}
+              cardType="cardType"
+              imgHeight="100%"
+              height="460px"
+              priceBottom="30px"
+              priceColor={COLORS.c1b1b1b}
+            />
+          ))}
+        </ProductListStyle>
+        <ProductListStyle>
+          {products.map((product) => (
+            <CardTypeItem
+              key={product.id}
+              item={product}
+              cardType="cardType"
+              imgHeight="100%"
+              height="460px"
+              priceBottom="30px"
+              priceColor={COLORS.c1b1b1b}
+            />
+          ))}
+        </ProductListStyle>
+      </div>
+    </div>
+  )
+}
+
+const ProductListStyle = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 13px;
+`
+
+export default MonthProductList
--- src/pages/Product/Theme.tsx
+++ src/pages/Product/Theme.tsx
@@ -4,6 +4,10 @@ import Title from '@src/components/common/Title'
 import ProductList from '@src/components/Group/ProductList'
 import CategoryList from '@src/components/Group/CategoryList'
 import Banner from '@src/components/Home/Banner'
+import styled from 'styled-components'
+import { COLORS, FONTSIZE, FONTWEGHT } from '@src/styles/root'
+import MoreBtn from '@src/components/Home/MoreBtn'
+import MonthProductList from '@src/components/Theme/MonthProductList'
 
 type Props = {}
 
@@ -17,17 +21,84 @@ const Theme = (props: Props) => {
       </Inner>
       <div style={{ width: '100%', height: '569px', backgroundColor: 'tomato' }}>슬라이더 영역</div>
       <Inner>
-        <ProductList title="TOP 10 인기 그룹별 여행" />
-        <ProductList title="방금 뜬 그룹별 여행" />
-        <CategoryList title="여자끼리" />
-        <CategoryList title="남자끼리" />
-        <Banner image="event_banner2" marginTop="100px" marginBottom="100px" type={1} />
-        <CategoryList title="5070끼리" />
-        <CategoryList title="가족끼리" />
-        <CategoryList title="누구든지" />
+        <CategoryList title="TOP 10 인기 테마별 여행" />
+        <Title margin="50px 0">
+          <h2>테마별 인기 여행</h2>
+          <div style={{ height: '696px', backgroundColor: 'tomato', marginTop: '50px' }}></div>
+        </Title>
+        <Banner image="event_banner2" marginTop="100px" marginBottom="20px" type={1} />
+        <ContainerStyle>
+          <EventCardStyle>
+            <ImageBoxStyle url="모로코_10일_썸네일">
+              <MoreBtn bgColor={COLORS.white} bottom="16px" right="18px" />
+            </ImageBoxStyle>
+            <EventInfo>
+              <p>14일 간의 모로코 시티&사막 투어</p>
+              <span>[5/8 출발] 선착 순 마감 중</span>
+            </EventInfo>
+          </EventCardStyle>
+          <EventCardStyle>
+            <ImageBoxStyle url="마레모_12일_썸네일">
+              <MoreBtn bgColor={COLORS.white} bottom="16px" right="18px" />
+            </ImageBoxStyle>
+            <EventInfo>
+              <p>마레모 12일 유적 탐방</p>
+              <span>[5/8 출발] 선착 순 마감 중</span>
+            </EventInfo>
+          </EventCardStyle>
+          <EventCardStyle>
+            <ImageBoxStyle url="아프리카7개국_18일_썸네일">
+              <MoreBtn bgColor={COLORS.white} bottom="16px" right="18px" />
+            </ImageBoxStyle>
+            <EventInfo>
+              <p>아프리카 8개국 27일 여행</p>
+              <span>[5/8 출발] 선착 순 마감 중</span>
+            </EventInfo>
+          </EventCardStyle>
+        </ContainerStyle>
+        <MonthProductList />
       </Inner>
     </div>
   )
 }
+
+const ContainerStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const EventCardStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const ImageBoxStyle = styled.div`
+  position: relative;
+  width: 380px;
+  height: 207px;
+  border-radius: 8px;
+  background-image: ${({ url }: { url: string }) => `url(/images/event/${url}.jpg)`};
+  background-size: 380px;
+  background-position: center;
+  background-repeat: no-repeat;
+  text-align: center;
+`
+
+const EventInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 18px 14px 33px;
+  p {
+    font-size: ${FONTSIZE.fz20};
+    font-weight: ${FONTWEGHT.fw500};
+  }
+  span {
+    font-weight: ${FONTWEGHT.fw500};
+    font-size: ${FONTSIZE.fz16};
+    color: ${COLORS.ca6a6a6};
+    line-height: 24px;
+  }
+`
 
 export default Theme
