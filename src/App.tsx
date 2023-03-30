5f11341a63e0f7c4467ab84c13734c9878d184c8
Wonny-ing <h.wonny11@gmail.com>
2023-03-31 02:20:14 +0900

    Fix: 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import styled from 'styled-components'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import { GlobalStyle } from './styles/GlobalStyle'
-// import ScrollTop from './utils/scrollTop'
+import ScrollTop from './utils/scrollTop'
 
 interface Props {}
 
 const App = (props: Props) => {
   return (
     <>
-      {/* <ScrollTop /> */}
+      <ScrollTop />
       <GlobalStyle />
       <Header />
       <Outlet />
---
+++ src/utils/count.ts
@@ -0,0 +1,7 @@
+const count = (constant: number[], start: number, end: number) => {
+  for (let i = start; i <= end; i++) {
+    constant.push(i)
+  }
+}
+
+export default count
