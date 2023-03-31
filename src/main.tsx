ef370223507bb530cfdb247f2bb1889b2758d195
team7 <hnhyerim@gmail.com>
2023-04-01 03:48:09 +0900

    Feature: Redux Provider 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import router from './router'
 import { RouterProvider } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
+import { Provider } from 'react-redux'
+import store from './reduxStore/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <CookiesProvider>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </CookiesProvider>
 )
