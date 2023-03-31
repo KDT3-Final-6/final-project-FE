e0f5d3e7b27fd43452ccdafccdb215b3b8645603
team7 <hnhyerim@gmail.com>
2023-04-01 04:37:58 +0900

    Style: 임시 상품 인터페이스

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,15 +1,24 @@
+// export interface IProduct {
+//   content: {
+//     productId: number,
+//     productName: string,
+//     productThumbnail: string,
+//     productPrice: number,
+//     productStatus: string,
+//     productContent: string,
+//     contentDetail: string
+//   }[],
+//   totalPages: number,
+//   totalElements: number,
+//   pageNumber: number,
+//   size:number,
+// }
+
 export interface IProduct {
-  content: {
-    productId: number,
-    productName: string,
-    productThumbnail: string,
-    productPrice: number,
-    productStatus: string,
-    productContent: string,
-    contentDetail: string
-  }[],
-  totalPages: number,
-  totalElements: number,
-  pageNumber: number,
-  size: number
+  id: number
+  title: string
+  image: string
+  hashs: string[]
+  price: number
+  heart: boolean
 }
