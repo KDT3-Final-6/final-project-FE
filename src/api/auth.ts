3d8decddb84f45f1c1a2af2e6c67e32a5bd6be57
team7 <hnhyerim@gmail.com>
2023-03-31 14:14:48 +0900

    Feature: 회원 (회원가입, 로그인, 로그아웃, 회원정보, 회원정보 수정, 회원탈퇴) api 설정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,15 +6,34 @@ interface ILogin{
   name: string
 }
 
+
+// 회원가입
+export const signup = async (data: object) => {
+  await axiosInstance.post(API_URL.members, data)
+}
+
+// 로그인
 export const login = async (data:object) => {
   const response:ILogin = await axiosInstance.post(API_URL.login, data)
   return response
 }
 
+// 로그아웃
 export const logout = async()=> {
-  await axiosInstance.post(API_URL.login)
+  await axiosInstance.post(API_URL.logout)
 }
 
+// 회원정보
 export const userInfo = async() => {
-  await axiosInstance.get(API_URL.user)
+  await axiosInstance.get(API_URL.members)
+}
+
+// 회원정보수정
+export const userInfoEdit = async () => {
+  await axiosInstance.patch(API_URL.members)
+}
+
+// 회원탈퇴
+export const userWithDrawal = async () => {
+  await axiosInstance.delete(API_URL.members)
 }
