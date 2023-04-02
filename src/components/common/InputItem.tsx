52dac3f9089137dff345d0a216df8220f6eea0c5
team7 <hnhyerim@gmail.com>
2023-04-02 20:25:37 +0900

    Feature: inputItem props 추가

--- src/components/common/InputItem.tsx
+++ src/components/common/InputItem.tsx
@@ -6,25 +6,41 @@ import { HighlightSpanStyle } from './Title'
 
 interface IInputItem {
   inputType?: string
+  type?: string
   title: string
   placeholder: string
   color?: string
   highlight?: string
   isDisabled?: boolean
+  register?: object
+  ariaInvalid?: boolean
+  errorMessage?: string
 }
 
 const InputItem = ({
   inputType = 'textInput',
+  type,
   title,
   placeholder,
   color = COLORS.c3ba1ff,
   highlight = '(필수입력)',
   isDisabled = false,
+  register,
+  ariaInvalid = false,
+  errorMessage = '',
 }: IInputItem) => {
   return (
-    <Input inputType={inputType} placeholder={placeholder} isDisabled={isDisabled}>
+    <Input
+      inputType={inputType}
+      type={type}
+      placeholder={placeholder}
+      isDisabled={isDisabled}
+      register={register}
+      ariaInvalid={ariaInvalid}
+    >
       <PAlignStyle>
         {title} {!isDisabled && <HighlightSpanStyle color={color}>{highlight}</HighlightSpanStyle>}
+        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
       </PAlignStyle>
     </Input>
   )
@@ -36,4 +52,8 @@ const PAlignStyle = styled.p`
   width: 100%;
   display: flex;
   justify-content: space-between;
+`
+
+const ErrorMessage = styled.p`
+  color: red;
 `
