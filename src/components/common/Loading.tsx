dd43903f2234ab013232ce69e0b479f2a7e00669
team7 <hnhyerim@gmail.com>
2023-04-07 00:13:25 +0900

    Design: 로딩 로티 추가

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,12 +1,21 @@
 import { RootState } from '@src/reduxStore/store'
+import Lottie from 'lottie-react'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
 
 const Loading = () => {
   const loading = useSelector((state: RootState) => state.loading)
   if (loading === false) return null
 
-  return <div>Loading</div>
+  return (
+    <LoadingStyle>
+      {/* <Lottie animationData={loading} loop={true} /> */}
+      loading
+    </LoadingStyle>
+  )
 }
 
 export default Loading
+
+const LoadingStyle = styled.div``
