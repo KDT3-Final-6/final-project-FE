067181b51bb2d7042ccef96f5dd56181c1aa5996
team7 <hnhyerim@gmail.com>
2023-04-09 20:05:54 +0900

    Design: notfound 페이지 디자인 수정

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,14 +4,23 @@ import styled from 'styled-components'
 import notFoundLottie from '@src/lotties/notFoundLottie.json'
 import { COLORS, FONTSIZE } from '@src/styles/root'
 import Title from '@src/components/common/Title'
+import Button from '@src/components/common/Button'
+import { ImHome } from 'react-icons/im'
+import { useNavigate } from 'react-router-dom'
+import PATH from '@src/constants/pathConst'
 
 interface Props {}
 
 const NotFound = (props: Props) => {
+  const navigate = useNavigate()
   return (
     <NotFoundStyle>
       <Lottie animationData={notFoundLottie} loop={true} />
-      <Title titleType="h1" title="NOT FOUND" fontSize={FONTSIZE.fz45} />
+      <Title titleType="h1" title="PAGE NOT FOUND" fontSize={FONTSIZE.fz45} />
+      <Button buttonType="skyblue" onClick={() => navigate(PATH.HOME)}>
+        <ImHome />
+        Home
+      </Button>
     </NotFoundStyle>
   )
 }
@@ -25,11 +34,22 @@ const NotFoundStyle = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin-top: -70px;
+  margin-top: -50px;
 
   div {
     width: 600px;
     text-align: center;
-    color: ${COLORS.primary};
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+
+    svg {
+      margin-right: 10px;
+      font-size: ${FONTSIZE.fz18};
+    }
   }
 `
