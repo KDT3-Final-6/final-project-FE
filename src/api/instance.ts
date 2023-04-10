4a47580b239cc6d9fd0d70e9c4bd1dfa3026021e
team7 <hnhyerim@gmail.com>
2023-04-10 15:42:00 +0900

    Feature: axios 인스턴스 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -52,6 +52,17 @@ const axiosApi = (url: string, forData: boolean) => {
     }
   )
 
+    instance.interceptors.request.use(
+    (config) => {
+      const token = getCookie('accessToken')
+      if (token) config.headers['Authorization'] = `Bearer ${token}`
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
   return instance
 }
 
