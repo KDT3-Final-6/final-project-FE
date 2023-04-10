ba6fb12cffd852ab5f5cb05a14bed967871bde72
team7 <hnhyerim@gmail.com>
2023-04-10 15:25:37 +0900

    Feature: axios 인스턴스 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,13 +1,16 @@
 import { getCookie } from '@src/utils/cookie'
 import axios from 'axios'
+import { login } from "./auth"
 
 const API_BASE_URL: string = import.meta.env.VITE_BASE_URL
 
+const token = getCookie('accessToken')
 const axiosApi = (url: string, forData: boolean) => {
   const instance = axios.create({
     baseURL: url,
     headers: {
       'Content-Type': forData ? 'multipart/form-data' : 'application/json',
+      'Authorization': `Bearer ${token}`
     },
   })
   instance.defaults.timeout = 5000
@@ -16,19 +19,36 @@ const axiosApi = (url: string, forData: boolean) => {
     (response) => {
       return response.data
     },
-    (error) => {
-      return Promise.reject(error)
-    }
-  )
+    async (error) => {
+      const {
+        config,
+        response: { status }
+      } = error
 
-  instance.interceptors.request.use(
-    (config) => {
-      const token = getCookie('accessToken')
-      if (token) config.headers['Authorization'] = `Bearer ${token}`
-      return config
-    },
-    (error) => {
-      return Promise.reject(error)
+      const originalRequest = config
+
+      if (status === 403) {
+        const accessToken = getCookie('accessToken')
+        const refreshToken = getCookie('refreshToken')
+
+        try {
+          const {data} = await login({
+            token: {accessToken, refreshToken}
+          })
+          const newAccessToken = data.token.accessToken
+          const newRefreshToken = data.token.refreshToken
+          originalRequest.headers = {
+            'Content-Type': forData ? 'multipart/form-data' : 'application/json',
+            'Authorization': `Bearer ${newAccessToken}`
+          }
+          localStorage.setItem('accessToken', newAccessToken)
+          localStorage.setItem('refreshToken', newRefreshToken)
+          return await axios(originalRequest)
+        } catch (error: any) {
+          new Error(error)
+        }
+      }
+        return Promise.reject(error)
     }
   )
 
