269856c8b8758941a139fac2c760a9ec77c5fb02
team7 <hnhyerim@gmail.com>
2023-04-12 01:50:06 +0900

    Refactor: 관리자 거래내역 rtk Query 적용 성공

--- src/pages/Admin/TransactionList.tsx
+++ src/pages/Admin/TransactionList.tsx
@@ -6,6 +6,7 @@ import Select from '@src/components/common/Select'
 import MESSAGES from '@src/constants/messages'
 import { ITransactionList } from '@src/interfaces/transaction'
 import Inner from '@src/layout/Inner'
+import { useGetTransactionsQuery } from '@src/reduxStore/api/adminTransactionApiSlice'
 import { hideLoading, showLoading } from '@src/reduxStore/loadingSlice'
 import { setModal } from '@src/reduxStore/modalSlice'
 import { COLORS, FONTSIZE, FONTWEGHT } from '@src/styles/root'
@@ -13,48 +14,48 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
-const TransactionList = () => {
+const Transaction = () => {
   const dispatch = useDispatch()
   const [page, setPage] = useState<number>(1)
 
-  const [transactions, setTransactions] = useState<ITransactionList>({
-    content: [],
-    totalPages: 0,
-    totalElements: 0,
-    pageNumber: 0,
-    size: 0,
-  })
+  // const [transactions, setTransactions] = useState<ITransactionList>({
+  //   content: [],
+  //   totalPages: 0,
+  //   totalElements: 0,
+  //   pageNumber: 0,
+  //   size: 0,
+  // })
 
-  useEffect(() => {
-    ;(async () => {
-      try {
-        dispatch(showLoading())
-        setTransactions(await getAdminTransactions(page))
-      } catch (error) {
-        dispatch(
-          setModal({
-            isOpen: true,
-            text: MESSAGES.ADMIN.TRANSACTION.error,
-            onClickOK: () => {
-              dispatch(setModal({ isOpen: false }))
-            },
-          })
-        )
-      } finally {
-        dispatch(hideLoading())
-      }
-    })()
-  }, [page])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     try {
+  //       dispatch(showLoading())
+  //       setTransactions(await getAdminTransactions(page))
+  //     } catch (error) {
+  //       dispatch(
+  //         setModal({
+  //           isOpen: true,
+  //           text: MESSAGES.ADMIN.TRANSACTION.error,
+  //           onClickOK: () => dispatch(setModal({ isOpen: false }))
+  //         })
+  //       )
+  //     } finally {
+  //       dispatch(hideLoading())
+  //     }
+  //   })()
+  // }, [page])
 
-  // const { data: transactions, isLoading, error } = useGetTransactionsQuery(page)
-  // if (isLoading) <>Loading</>
-  // if (error) {
-  //   setModal({
-  //     isOpen: true,
-  //     text: MESSAGES.ADMIN.TRANSACTION.error,
-  //     onClickOK: () => setModal({ isOpen: false }),
-  //   })
-  // }
+  const { data: transactions, isLoading, error } = useGetTransactionsQuery(page)
+  isLoading ? dispatch(showLoading()) : dispatch(hideLoading())
+  if (error) {
+    dispatch(
+      setModal({
+        isOpen: true,
+        text: MESSAGES.ADMIN.TRANSACTION.error,
+        onClickOK: () => dispatch(setModal({ isOpen: false })),
+      })
+    )
+  }
   const selectOptions = ['결제대기', '결제완료']
   const [currentValue, setCurrentValue] = useState<string>(selectOptions[0])
 
@@ -63,83 +64,75 @@ const TransactionList = () => {
   }
 
   return (
-    <>
-      {/* {error &&
-        setModal({
-          isOpen: true,
-          text: MESSAGES.ADMIN.TRANSACTION.error,
-          onClickOK: () => setModal({ isOpen: false }),
-        })} */}
-      <Inner>
-        <InputWrapStyle>
-          <Input
-            inputType="searchInput"
-            type="text"
-            width="925px"
-            height="56px"
-            placeholder="검색"
-            borderRadius="0"
-            bgColor="transparent"
-            borderColor={COLORS.c999}
-          />
-        </InputWrapStyle>
-        <CountStyle>
-          <span>신규주문</span>
-          <span>{transactions?.totalElements}</span>
-          <span>개</span>
-        </CountStyle>
-        <TransactionListStyle>
-          <TransactionTable>
-            <colgroup>
-              <col width="15%" />
-              <col width="" />
-              <col width="13%" />
-              <col width="5%" />
-              <col width="" />
-              <col width="20%" />
-              <col width="10%" />
-            </colgroup>
-            <Thead>
-              <tr>
-                <th>주문번호/시각</th>
-                <th>주문상품</th>
-                <th>상품금액</th>
-                <th>수량</th>
-                <th>
-                  <Select
-                    options={selectOptions}
-                    currentValue={currentValue}
-                    setCurrentValue={setCurrentValue}
-                    height="24px"
-                    borderColor="transparent"
-                    borderRadius="0"
-                    fontSize="16px"
-                    arrowImg="/images/icons/bottom-arrow1.png"
-                  />
-                </th>
-                <th>결제내역</th>
-                <th>관리</th>
-              </tr>
-            </Thead>
-            <Tbody>
-              {transactions?.content
-                .filter((item) => item.orderStatus === currentValue)
-                .map((transaction) => (
-                  <TransactionCon key={transaction.purchasedProductId} transaction={transaction} />
-                ))}
-            </Tbody>
-          </TransactionTable>
-          <Paginate
-            totalElements={transactions?.totalPages || 0}
-            changePageHandler={changePageHandler}
-          />
-        </TransactionListStyle>
-      </Inner>
-    </>
+    <Inner>
+      <InputWrapStyle>
+        <Input
+          inputType="searchInput"
+          type="text"
+          width="925px"
+          height="56px"
+          placeholder="검색"
+          borderRadius="0"
+          bgColor="transparent"
+          borderColor={COLORS.c999}
+        />
+      </InputWrapStyle>
+      <CountStyle>
+        <span>신규주문</span>
+        <span>{transactions?.totalElements}</span>
+        <span>개</span>
+      </CountStyle>
+      <TransactionListStyle>
+        <TransactionTable>
+          <colgroup>
+            <col width="15%" />
+            <col width="" />
+            <col width="13%" />
+            <col width="5%" />
+            <col width="" />
+            <col width="20%" />
+            <col width="10%" />
+          </colgroup>
+          <Thead>
+            <tr>
+              <th>주문번호/시각</th>
+              <th>주문상품</th>
+              <th>상품금액</th>
+              <th>수량</th>
+              <th>
+                <Select
+                  options={selectOptions}
+                  currentValue={currentValue}
+                  setCurrentValue={setCurrentValue}
+                  height="24px"
+                  borderColor="transparent"
+                  borderRadius="0"
+                  fontSize="16px"
+                  arrowImg="/images/icons/bottom-arrow1.png"
+                />
+              </th>
+              <th>결제내역</th>
+              <th>관리</th>
+            </tr>
+          </Thead>
+          <Tbody>
+            {transactions?.content
+              .filter((item) => item.orderStatus === currentValue)
+              .map((transaction) => (
+                <TransactionCon key={transaction.purchasedProductId} transaction={transaction} />
+              ))}
+          </Tbody>
+        </TransactionTable>
+        <Paginate
+          totalElements={transactions?.totalPages || 0}
+          changePageHandler={changePageHandler}
+        />
+      </TransactionListStyle>
+    </Inner>
   )
 }
 
-export default TransactionList
+export default Transaction
 
 const TransactionTable = styled.table`
   width: 100%;
