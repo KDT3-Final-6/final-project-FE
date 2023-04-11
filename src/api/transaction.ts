2ac5e2e064ebbfb67faf04602f0180304390c74b
team7 <hnhyerim@gmail.com>
2023-04-12 01:04:53 +0900

    Feature: 관리자 거래내역 승인 api 생성

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -1,9 +1,15 @@
 import API_URL from '@src/constants/apiUrlConst'
 import { axiosInstance } from './instance'
-import ITransaction from "@src/interfaces/transaction"
+import { ITransaction, ITransactionApproval } from "@src/interfaces/transaction"
 
 /** 관리자 거래내역 API */
 export const getAdminTransactions = async (page: number = 1) => {
   const data: ITransaction = await axiosInstance.get(`${API_URL.admin_transactions}?page=${page}`)
+  return data
+}
+
+/** 관리자 거래내역 승인 API */
+export const getAdminTransactionsApproval = async (orderId: number) => {
+  const data: ITransactionApproval = await axiosInstance.get(`${API_URL.admin_transactions_approval}/${orderId}`)
   return data
 }
