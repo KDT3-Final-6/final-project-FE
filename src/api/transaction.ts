cf4f6e78bc76a77514cd3ce326848b6681a99e29
team7 <hnhyerim@gmail.com>
2023-04-12 01:15:36 +0900

    Fix: Deploy Error resolved

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -1,10 +1,10 @@
 import API_URL from '@src/constants/apiUrlConst'
 import { axiosInstance } from './instance'
-import { ITransaction, ITransactionApproval } from "@src/interfaces/transaction"
+import { ITransactionApproval, ITransactionList } from "@src/interfaces/transaction"
 
 /** 관리자 거래내역 API */
 export const getAdminTransactions = async (page: number = 1) => {
-  const data: ITransaction = await axiosInstance.get(`${API_URL.admin_transactions}?page=${page}`)
+  const data: ITransactionList = await axiosInstance.get(`${API_URL.admin_transactions}?page=${page}`)
   return data
 }
 
